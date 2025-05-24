5353de11a0b7772c13838cf5c9ff36606abccd17
SmitRupani <smit.24bcs10256@sst.scaler.com>
2025-05-24 21:39:58 +0530

    cleaned structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 // import Landing from './components/Landing';
@@ -12,22 +12,31 @@ import Footer from './components/Footer';
 import Search from './components/Search';
 import DeadlineWindow from './components/DeadlineWindow';
 import Landing from './components/Landing'
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 100,
+    });
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
       <div className="flex-grow">
       <Routes>
-        {/* <Route path="/" element={<Landing />} /> */}
-        <Route path="/home" element={<Home />} />
         <Route path="/deadline" element={<DeadlineWindow />} />
         <Route path='/dashboard' element={<Dashboard/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<Signup/>}></Route>
         <Route path='/announcements' element={<Announcements/>}></Route>
         <Route path='/search' element={<Search/>}></Route>
-        <Route path="/landing" element={<Landing/>}></Route>
+        <Route path="/" element={<Landing/>}></Route>
       </Routes>
       </div>
       <Footer/>
