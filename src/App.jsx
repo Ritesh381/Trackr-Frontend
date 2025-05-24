6e3835801305626f8951864d1c2d79346cd30594
Jenil <langaliyajenil@gmail.com>
2025-05-24 20:25:19 +0530

    Added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import "./App.css"
 import Home from './components/Home'
 import NavBar from './components/NavBar'
+import Landing from './components/Landing'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 800 , once: true , offset:100 });
+  }, []);
   return (
     <div>
       <NavBar></NavBar>
-      <Home></Home>
+      <Landing></Landing>
     </div>
   )
 }
